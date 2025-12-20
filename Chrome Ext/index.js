76ec7fa163a9fd8308d451e5d0a109f91d53a297
lokhacodes <sumaiyasonali421@gmail.com>
2025-12-20 23:44:58 +0600

    working with innerhtml

--- Chrome Ext/index.js
+++ Chrome Ext/index.js
@@ -1,13 +1,18 @@
 
-let myLeads=[]
+let myLeads= ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
-
+const ulEl = document.getElementById("ul-el")
+console.log(ulEl)
 inputBtn.addEventListener("click", function(){
     myLeads.push(inputEl.value)
     console.log(myLeads)
 })
 
 
-    for(let i=0; i<=myLeads.length;i++){
-      console.log(myLeads[i])}
+    for(let i=0; i< myLeads.length;i++){
+      ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+   // document.createElement("li") }
+   // li.textContent = myLeads[i]
+   // ulEl.ariaExpanded(li)
+    
