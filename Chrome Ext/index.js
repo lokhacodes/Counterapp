6344262072d858eea9a2e68d68f82e614ae00505
lokhacodes <sumaiyasonali421@gmail.com>
2025-12-22 22:44:39 +0600

    render function

--- Chrome Ext/index.js
+++ Chrome Ext/index.js
@@ -1,37 +1,48 @@
 
 let myLeads= []
-
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
-
-localStorage.clear()
- let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
- console.log(leadsFromLocalStorage)
-inputBtn.addEventListener("click", function(){
-    myLeads.push(inputEl.value)
-   // ulEl.innerHTML+= "<li>" +inputEl.value + "</li>"
-    inputEl.value = ""
-    localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    
-    renderLeads()
-    console.log(localStorage.getItem("myLeads"))
-})
+const deleteBtn = document.getElementById("delete-btn")
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
 
-//localStorage.setItem("myLeads", "www.examplelead.com")
-//console.log(localStorage.getItem("myLeads"))
-
-function renderLeads() {
+if(leadsFromLocalStorage){
+  myLeads= leadsFromLocalStorage
+  render(myLeads)
+  }
+ 
+function render(leads) {
     let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
+    for (let i = 0; i < leads.length; i++) {
         listItems += `
         <li>
-            <a target='_blank' href='${myLeads[i]}'>
-                ${myLeads[i]}
+            <a target='_blank' href='${leads[i]}'>
+                ${leads[i]}
             </a>
         </li>
       `
     }
     ulEl.innerHTML = listItems
 }
+
+ deleteBtn.addEventListener("dblclick", function(){
+    console.log("double clicked!")
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+ })  
+ inputBtn.addEventListener("click", function(){
+    myLeads.push(inputEl.value)
+   // ulEl.innerHTML+= "<li>" +inputEl.value + "</li>"
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    
+    render(myLeads)
+    
+})
+
+
+//localStorage.setItem("myLeads", "www.examplelead.com")
+//console.log(localStorage.getItem("myLeads"))
+
